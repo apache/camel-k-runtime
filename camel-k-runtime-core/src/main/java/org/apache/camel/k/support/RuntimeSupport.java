e07ef27c54ecf4e86fccb0dc04a8eaa9ae3d0a7a
lburgazzoli <lburgazzoli@gmail.com>
2019-04-18 11:22:19 +0200

    customizer: honour cutomizer list property/env

--- camel-k-runtime-core/src/main/java/org/apache/camel/k/support/RuntimeSupport.java
+++ camel-k-runtime-core/src/main/java/org/apache/camel/k/support/RuntimeSupport.java
@@ -25,8 +25,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.camel.CamelContext;
@@ -41,46 +46,80 @@ import org.apache.camel.k.adapter.Introspection;
 import org.apache.camel.spi.FactoryFinder;
 import org.apache.camel.spi.RestConfiguration;
 import org.apache.camel.util.ObjectHelper;
+import org.apache.camel.util.StringHelper;
 import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public final class RuntimeSupport {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RuntimeSupport.class);
 
     private RuntimeSupport() {
     }
 
-    public static void configureContext(CamelContext context, Runtime.Registry registry) {
-        try {
-            FactoryFinder finder = context.getFactoryFinder(Constants.CONTEXT_CUSTOMIZER_RESOURCE_PATH);
-            String customizerIDs = System.getenv().getOrDefault(Constants.ENV_CAMEL_K_CUSTOMIZERS, "");
+    public static List<ContextCustomizer> configureContext(CamelContext context, Runtime.Registry registry) {
+        List<ContextCustomizer> appliedCustomizers = new ArrayList<>();
+        Set<String> customizers = lookupCustomizerIDs(context);
 
-            if (ObjectHelper.isEmpty(customizerIDs)) {
-                PropertiesComponent component = context.getComponent("properties", PropertiesComponent.class);
-                Properties properties = component.getInitialProperties();
-
-                if (properties != null) {
-                    customizerIDs = properties.getProperty(Constants.PROPERTY_CAMEL_K_CUSTOMIZER, "");
-                }
+        // this is to initialize all customizers that might be already present in
+        // the context injected by other means.
+        for (Map.Entry<String, ContextCustomizer> entry: context.getRegistry().findByTypeWithName(ContextCustomizer.class).entrySet()) {
+            if (!customizers.remove(entry.getKey())) {
+                continue;
             }
 
-            if  (ObjectHelper.isNotEmpty(customizerIDs)) {
-                for (String customizerId : customizerIDs.split(",", -1)) {
-                    configureContext(context, customizerId, (ContextCustomizer) finder.newInstance(customizerId), registry);
-                }
-            }
-        } catch (NoFactoryAvailableException e) {
-            // ignored
+            applyCustomizer(context, entry.getKey(), entry.getValue(), registry);
+
+            appliedCustomizers.add(entry.getValue());
         }
 
-        //this is to initialize all customizers that might be already present in the context injected by other means.
-        context.getRegistry().findByTypeWithName(ContextCustomizer.class).forEach(
-            (customizerId, customizer) -> configureContext(context, customizerId, customizer, registry)
-        );
+        try {
+            FactoryFinder finder = context.getFactoryFinder(Constants.CONTEXT_CUSTOMIZER_RESOURCE_PATH);
+
+            for (String customizerId : customizers) {
+                ContextCustomizer customizer = (ContextCustomizer) finder.newInstance(customizerId);
+                applyCustomizer(context, customizerId, customizer, registry);
+
+                appliedCustomizers.add(customizer);
+            }
+        } catch (NoFactoryAvailableException e) {
+            throw new RuntimeException(e);
+        }
+
+        return appliedCustomizers;
     }
 
-    public static void configureContext(CamelContext context, String customizerId, ContextCustomizer customizer, Runtime.Registry registry) {
+    public static void applyCustomizer(CamelContext context, String customizerId, ContextCustomizer customizer, Runtime.Registry registry) {
+        ObjectHelper.notNull(customizer, "customizer");
+        StringHelper.notEmpty(customizerId, "customizerId");
+
+        LOGGER.info("Apply ContextCustomizer with id={} and type={}", customizerId, customizer.getClass().getName());
+
         bindProperties(context, customizer, "customizer." + customizerId + ".");
         customizer.apply(context, registry);
+    }
+
+    public static Set<String> lookupCustomizerIDs(CamelContext context) {
+        Set<String> customizers = new TreeSet<>();
+
+        String customizerIDs = System.getenv().getOrDefault(Constants.ENV_CAMEL_K_CUSTOMIZERS, "");
+        if (ObjectHelper.isEmpty(customizerIDs)) {
+            PropertiesComponent component = context.getComponent("properties", PropertiesComponent.class);
+            Properties properties = component.getInitialProperties();
+
+            if (properties != null) {
+                customizerIDs = properties.getProperty(Constants.PROPERTY_CAMEL_K_CUSTOMIZER, "");
+            }
+        }
+
+        if  (ObjectHelper.isNotEmpty(customizerIDs)) {
+            for (String customizerId : customizerIDs.split(",", -1)) {
+                customizers.add(customizerId);
+            }
+        }
+
+        return customizers;
     }
 
     public static void configureRest(CamelContext context) {
