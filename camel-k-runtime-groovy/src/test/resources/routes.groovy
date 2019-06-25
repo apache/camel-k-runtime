50adeda2269edaa7376c0cebb7cc10fbbc044240
lburgazzoli <lburgazzoli@gmail.com>
2019-06-25 13:33:59 +0200

    chekstyle(license): fix violations

--- camel-k-runtime-groovy/src/test/resources/routes.groovy
+++ camel-k-runtime-groovy/src/test/resources/routes.groovy
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,6 +14,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 from('timer:tick')
     .to('log:info')
