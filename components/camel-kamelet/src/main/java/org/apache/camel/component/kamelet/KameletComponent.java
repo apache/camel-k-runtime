8d072129e967a7f97df0ef1446ed1e1c69222b2c
Claus Ibsen <claus.ibsen@gmail.com>
2020-11-12 21:11:33 +0100

    Fixed PMD issue

--- components/camel-kamelet/src/main/java/org/apache/camel/component/kamelet/KameletComponent.java
+++ components/camel-kamelet/src/main/java/org/apache/camel/component/kamelet/KameletComponent.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.camel.AfterPropertiesConfigured;
