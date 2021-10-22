178083668d46c00e6cb38f5997f12efc31ed49bc
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-10-22 18:24:43 +0200

    fix: move tests classes to a dedicate package to avoid split package warning

--- itests/camel-k-itests-loader-jsh/src/main/java/org/apache/camel/k/loader/jsh/quarkus/it/JshApplication.java
+++ itests/camel-k-itests-loader-jsh/src/main/java/org/apache/camel/k/loader/jsh/quarkus/it/JshApplication.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.k.loader.jsh.quarkus;
+package org.apache.camel.k.loader.jsh.quarkus.it;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
