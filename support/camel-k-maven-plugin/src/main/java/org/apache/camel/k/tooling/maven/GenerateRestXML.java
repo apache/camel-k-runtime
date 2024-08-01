7c7cc475835ddc7452c1669b936730f435042504
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2024-08-01 10:06:17 +0200

    chore: bump to CQ 3.13.0

--- support/camel-k-maven-plugin/src/main/java/org/apache/camel/k/tooling/maven/GenerateRestXML.java
+++ support/camel-k-maven-plugin/src/main/java/org/apache/camel/k/tooling/maven/GenerateRestXML.java
@@ -16,20 +16,15 @@
  */
 package org.apache.camel.k.tooling.maven;
 
-import java.io.FileInputStream;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import io.apicurio.datamodels.Library;
-import io.apicurio.datamodels.models.openapi.OpenApiDocument;
-import io.apicurio.datamodels.models.util.JsonUtil;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.generator.openapi.RestDslXmlGenerator;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -65,19 +60,7 @@ class GenerateRestXML extends AbstractMojo {
         }
 
         try {
-            JsonFactory factory = null;
-            if (inputFile.endsWith(".yaml") || inputFile.endsWith(".yml")) {
-                factory = new YAMLFactory();
-            }
-
-            ObjectMapper mapper = new ObjectMapper(factory);
-            mapper.findAndRegisterModules();
-
-            FileInputStream fis = new FileInputStream(inputFile);
-
-            JsonNode node = mapper.readTree(fis);
-            OpenApiDocument document = (OpenApiDocument) Library.readDocument(JsonUtil.toObject(node));
-
+            OpenAPI document = new OpenAPIV3Parser().read(inputFile);
             final Writer writer;
 
             if (outputFile != null) {
